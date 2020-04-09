303364bc41773ef2b57952e62cd4aae5dcbae106
lülü <einKnie@gmx.at>
2020-04-09 10:54:27 +0200

    fixed a typo in popup

--- popup/popup.js
+++ popup/popup.js
@@ -2,32 +2,29 @@
 // functions here shouldbe called when the addon's popup is called and suspend-button pressed
 
 
-var default_suspend = "Suspend";
-var default_resume = "Resume";
-var suspendButton = document.querySelector('[class="button suspend"]');
-var suspendState = false;
+const default_suspend = "Suspend";
+const default_resume  = "Resume";
+const suspendButton   = document.querySelector('[class="button suspend"]');
+var   suspendState    = false;
 
 fetchLocalState()
   .then(function() {
     setButtonText(suspendState);
     document.querySelector('[class="button suspend"]').addEventListener("click", onSuspendClicked);
     document.querySelector('[class="button settings"]').addEventListener("click", onSettingsClicked);
-  }, onError());
+  }, onError);
 
 
 function setButtonText() {
   if (suspendButton != null) {
-    console.log("button found");
     if (suspendState) suspendButton.textContent = default_resume;
     else              suspendButton.textContent = default_suspend;
   } else {
-    console.log("no button found");
+    onError("no button found");
   }
 }
 
 function onSuspendClicked() {
-  console.log("suspend button clicked");
-
   suspendState = !suspendState;
   setButtonText(suspendState);
   browser.storage.local.set({suspended: suspendState});
@@ -35,26 +32,18 @@ function onSuspendClicked() {
 
 
 function onSettingsClicked() {
-  console.log("settings button clicked");  
   browser.runtime.sendMessage({settings: true});
   window.close();
 }
 
 function fetchLocalState() {
-  return new Promise((resolve, reject) => {
-    var applySetting = function(pref) {
-      console.log("applying settings");
-      if (pref.suspended) {
-        suspendState = pref.suspended;
-      } else {
-        // no setting yet
-        suspendState = false;
-      }
+  return new Promise((resolve) => {
+    let applySetting = function(pref) {
+      suspendState = pref.suspended || false;
       setButtonText(suspendState);
       resolve("success");
     };
 
-    console.log("state applied");
     browser.storage.local.get("suspended")
       .then(applySetting, onError);
   });
@@ -63,7 +52,3 @@ function fetchLocalState() {
 function onError(e) {
   console.log("error: " + e);
 }
-
-console.log("popup opened");
-
-
